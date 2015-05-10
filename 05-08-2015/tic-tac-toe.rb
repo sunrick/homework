aad9198532794628cceebdd14deb63e5e9a4c5c8
Rickard Sunden <rickard.sunden@outlook.com>
2015-05-10 12:56:03 -0400

    Added random computer functionality

--- 05-08-2015/tic-tac-toe.rb
+++ 05-08-2015/tic-tac-toe.rb
@@ -21,6 +21,24 @@ def play_game?
   result
 end
 
+def play_computer?
+  result = nil
+  puts "Do you want to play against another player or the computer? p/c"
+  user_input = gets.chomp
+  until ['p','c'].include?(user_input.downcase)
+    puts "Sorry I didn't understand...please enter again: p/c"
+    user_input = gets.chomp
+  end
+  if user_input == "p"
+    result = false
+    puts "You have chosen to play an another player!"
+  else
+    result = true
+    puts "You have chosen to play the computer!"
+  end
+  result
+end
+
 def show_board(board)
   board = board.each_slice(3).to_a
   board.each do |row|
@@ -29,11 +47,13 @@ def show_board(board)
 end
 
 def whose_turn?(turn_count)
+  result = nil
   if turn_count.odd?
-    "player1"
+    result = "player1"
   else
-    "player2"
+    result = "player2"
   end
+  result
 end
 
 def get_player_input(player, board)
@@ -44,6 +64,13 @@ def get_player_input(player, board)
     user_input = gets.chomp.to_i
   end
   user_input
+end
+
+def choose_computer_move(player1_moves, player2_moves)
+  result = nil
+  possible_moves = (1..9).to_set
+  available_moves = (player1_moves + player2_moves) ^ possible_moves
+  result = available_moves.to_a.sample
 end
 
 def input_valid?(player_input, board)
@@ -86,10 +113,8 @@ def player_win?(player1_moves, player2_moves, winning_combos)
 end
 
 def game_complete?(turn_count, player1_moves, player2_moves, winning_combos)
-  
   result = nil
   player_win = player_win?(player1_moves, player2_moves, winning_combos)
-
   if player_win == "Player 1"
     puts "#{player_win} wins!"
     result = true
@@ -108,39 +133,46 @@ def game_complete?(turn_count, player1_moves, player2_moves, winning_combos)
 end
 
 def board_reset
-  board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  board = (1..9).to_a
+end
+
+def player_move(is_computer, player1_moves, player2_moves, current_player, board)
+  result = nil
+  if current_player == "player2"
+    if is_computer
+      result = choose_computer_move(player1_moves, player2_moves)
+    else
+      result = get_player_input(current_player, board)
+    end
+  else
+    result = get_player_input(current_player, board)
+  end
+  result
 end
 
 
-def tic_tac_toe(board, winning_combos)
-  
+def tic_tac_toe(board, winning_combos, is_play_computer)
   player1_moves = Set.new
   player2_moves = Set.new
   turn_count = 1
-
   show_board(board)
-
   until game_complete?(turn_count, player1_moves, player2_moves, winning_combos)
-    
     current_player = whose_turn?(turn_count)
-    player_input = get_player_input(current_player, board)
-    
+    the_move = player_move(is_play_computer, player1_moves, player2_moves, current_player, board)
     if current_player == "player1"
-      player1_moves << player_input
+      player1_moves << the_move
     else
-      player2_moves << player_input
+      player2_moves << the_move
     end
-
-    board = update_board(board, player_input, current_player)
+    board = update_board(board, the_move, current_player)
     show_board(board)
-
     turn_count += 1
   end
 end
 
 def main(board, winning_combos)
   while play_game?
-    tic_tac_toe(board, winning_combos)
+    tic_tac_toe(board, winning_combos, play_computer?)
     board = board_reset
   end
 end
