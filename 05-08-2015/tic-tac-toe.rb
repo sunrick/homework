2a656ce533d24653b3104dd5289c5f18e8301baf
Rickard Sunden <rickard.sunden@outlook.com>
2015-05-10 11:33:22 -0400

    Completely reworked version

--- 05-08-2015/tic-tac-toe.rb
+++ 05-08-2015/tic-tac-toe.rb
@@ -1,126 +1,148 @@
 require 'pry'
+require 'set'
 
-board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+winning_combos = board.each_slice(3).to_a + [[1,4,7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+winning_combos = winning_combos.to_set
 
 def play_game?
+  result = nil
   puts "Do you want to play tic-tac-toe? y/n"
   user_input = gets.chomp
   until ['y','n'].include?(user_input.downcase)
     puts "Sorry I didn't understand...please enter again: y/n"
     user_input = gets.chomp
   end
+  if user_input == "y"
+    result = true
+  else
+    result = false
+  end
+  result
 end
 
 def show_board(board)
+  board = board.each_slice(3).to_a
   board.each do |row|
     puts row.join
   end
 end
 
-def user_move
-  puts "Pick a move:"
-  user_input = gets.chomp.to_i
-end
-
-def update_board(board, user_move, user_letter)
-  board = board.flatten
-  if board.include?(user_move)
-    board[board.index(user_move)] = user_letter
-  else
-  end
-  board = board.each_slice(3).to_a
-end
-
-#returns letter X for player 1, 0 player
 def whose_turn?(turn_count)
   if turn_count.odd?
-    result = "X"
+    "player1"
   else
-    result = "O"
+    "player2"
   end
-  result
 end
 
-def game_finished?(board)
-  result = false
-  if vertical_check(board) || horizontal_check(board) || diagonal_check(board)
-    result = true
+def get_player_input(player, board)
+  puts "Pick a move #{player}: "
+  user_input = gets.chomp.to_i
+  until input_valid?(user_input, board)
+    puts "Invalid input..."
+    user_input = gets.chomp.to_i
   end
-  result
+  user_input
 end
 
-def vertical_check(board)
-  result = false
-  board = board.flatten
-  if unique_array?(board.values_at(0,3,6)) || unique_array?(board.values_at(1,4,7)) || unique_array?(board.values_at(2,5,8))
-    result = true
-  end
-  result
-end
-
-def diagonal_check(board)
-  result = false
-  board = board.flatten
-  if unique_array?(board.values_at(0,4,8)) || unique_array?(board.values_at(2,4,6))
-    result = true
-  end
-  result
-end
-
-def horizontal_check(board)
-  result = false
-  board.each do |row|
-    if unique_array?(row)
-      result = true
-      break
-    end
-  end
-  result
-end
-
-def unique_array?(array)
-  result = false
-  if array.uniq.length == 1
-    result = true
-  end
-  result
-end
-
-def tic_tac_toe (board)
-  turn_count = 1
-  while turn_count < 10
-    if !game_finished?(board)
-      board = update_board(board, user_move, whose_turn?(turn_count))
-      show_board(board)
-      turn_count += 1
+def input_valid?(player_input, board)
+  result = nil
+  if (1..9).to_a.include?(player_input)
+    if board.include?(player_input) == false
+      result = false
     else
-      turn_count = 10
+      result = true
+    end
+  else
+    result = false
+  end
+  result
+end
+
+def update_board(board, player_input, current_player)
+    board[board.index(player_input)] = player_letter(current_player)
+    board
+end
+
+def player_letter(current_player)
+  if current_player == "player1"
+    "X"
+  else
+    "O"
+  end
+end
+
+def player_win?(player1_moves, player2_moves, winning_combos)
+  result = nil
+  winning_combos.each do |combo|
+    if combo.to_set.subset?(player1_moves)
+      result = "Player 1"
+    elsif combo.to_set.subset?(player2_moves)
+      result = "Player 2"
     end
   end
-  puts "The game is over!"
+  result
 end
 
-def main(board)
-  play_game?
+def game_complete?(turn_count, player1_moves, player2_moves, winning_combos)
+  
+  result = nil
+  player_win = player_win?(player1_moves, player2_moves, winning_combos)
+
+  if player_win == "Player 1"
+    puts "#{player_win} wins!"
+    result = true
+  elsif player_win == "Player 2"
+    puts "#{player_win} wins!"
+    result = true
+  else
+    if turn_count == 10 #turn_count starts at 1
+      puts "Game over, nobody wins!"
+      result = true
+    else
+      result = false
+    end
+  end
+  result
+end
+
+def board_reset
+  board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+end
+
+
+def tic_tac_toe(board, winning_combos)
+  
+  player1_moves = Set.new
+  player2_moves = Set.new
+  turn_count = 1
+
   show_board(board)
-  tic_tac_toe(board)
+
+  until game_complete?(turn_count, player1_moves, player2_moves, winning_combos)
+    
+    current_player = whose_turn?(turn_count)
+    player_input = get_player_input(current_player, board)
+    
+    if current_player == "player1"
+      player1_moves << player_input
+    else
+      player2_moves << player_input
+    end
+
+    board = update_board(board, player_input, current_player)
+    show_board(board)
+
+    turn_count += 1
+  end
 end
 
-
-# winning combinations
-
-#horizontal
-# [1,2,3]
-# [4,5,6]
-# [7,8,9]
-# #vertical
-# [1,4,7]
-# [2,5,8]
-# [3,6,9]
-# #diagonal
-# [1,5,9]
-# [2,3,7]
-
-
+def main(board, winning_combos)
+  while play_game?
+    tic_tac_toe(board, winning_combos)
+    board = board_reset
+  end
+end
 
 binding.pry
