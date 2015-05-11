3e0b35288a2c948aa3d17ecc29df5ce2e3734b48
Rickard Sunden <rickard.sunden@outlook.com>
2015-05-11 16:05:01 -0400

    Refactored player move function

--- 05-08-2015/tic-tac-toe.rb
+++ 05-08-2015/tic-tac-toe.rb
@@ -1,6 +1,7 @@
 require 'pry'
 require 'set'
 
+#comment
 board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 winning_combos = board.each_slice(3).to_a + [[1,4,7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 WINS = winning_combos.to_set
