0b0d5e136add697e1993fd2593eb4f92a9b1eaea
Rickard Sunden <rickard.sunden@outlook.com>
2015-05-11 15:55:26 -0400

    More refactoring

--- 05-08-2015/tic-tac-toe.rb
+++ 05-08-2015/tic-tac-toe.rb
@@ -11,20 +11,6 @@ def play_game?
   result = user_input == "Y" ? true : false
 end
 
-def question_answer(question, regex)
-  puts dash_line
-  puts question
-  puts dash_line
-  result = gets.chomp
-  until result =~ regex
-    puts dash_line
-    puts "Sorry your input is not valid..."
-    puts dash_line
-    result = gets.chomp
-  end
-  result.upcase
-end
-
 def play_computer?
   question = "Do you want to play against another player or the computer? p/c"
   user_input = question_answer(question, /^[pc]$/i)
@@ -41,6 +27,20 @@ def play_computer?
   end
 end
 
+def question_answer(question, regex)
+  puts dash_line
+  puts question
+  puts dash_line
+  result = gets.chomp
+  until result =~ regex
+    puts dash_line
+    puts "Sorry your input is not valid..."
+    puts dash_line
+    result = gets.chomp
+  end
+  result.upcase
+end
+
 def show_board(board)
   puts
   board = board.each_slice(3).to_a
@@ -52,10 +52,6 @@ end
 
 def dash_line
   result = "-"*80
-end
-
-def whose_turn?(turn_count)
-  turn_count.odd? ? "player1" : "player2"
 end
 
 def choose_move(player, board)
@@ -76,11 +72,7 @@ def update_board(board, player_input, current_player)
 end
 
 def player_letter(current_player)
-  if current_player == "player1"
-    "X"
-  else
-    "O"
-  end
+  current_player == "player1" ? "X" : "O"
 end
 
 def player_win?(player1_moves, player2_moves)
@@ -95,33 +87,27 @@ def player_win?(player1_moves, player2_moves)
   result
 end
 
-def game_complete?(player1_moves, player2_moves, board)
-  player_win = player_win?(player1_moves, player2_moves)
-  if player_win == "Player 1"
-    puts dash_line
-    puts "#{player_win} wins!"
-    puts dash_line
-    result = true
-  elsif player_win == "Player 2"
-    puts dash_line
-    puts "#{player_win} wins!"
-    puts dash_line
-    result = true
-  else
-    if draw?(board) #turn_count starts at 1
-      puts dash_line
-      puts "Game over, nobody wins!"
-      puts dash_line
-      result = true
-    else
-      result = false
-    end
-  end
-  result
-end
-
 def draw?(board)
   board.all? {|x| x.is_a? String}
+end
+
+def game_complete?(player1_moves, player2_moves, board)
+  player_win = player_win?(player1_moves, player2_moves)
+  if player_win == "Player 1" || player_win == "Player 2"
+    puts dash_line
+    puts "#{player_win} wins!"
+    puts dash_line
+    show_board(board)
+    result = true
+  elsif draw?(board)
+    puts dash_line
+    puts "Game over, nobody wins!"
+    puts dash_line
+    show_board(board)
+    result = true
+  else
+    result = false
+  end
 end
 
 def player_move(is_computer, player1_moves, player2_moves, current_player, board)
@@ -140,38 +126,28 @@ def player_move(is_computer, player1_moves, player2_moves, current_player, board
   result
 end
 
-def board_reset
-  board = (1..9).to_a
-end
-
-def tic_tac_toe(board, is_play_computer)
-  player1_moves = Set.new
-  player2_moves = Set.new
-  show_board(board)
-  current_player = "player1"
-  current_letter = "X"
-  until game_complete?(player1_moves, player2_moves, board)
-    the_move = player_move(is_play_computer, player1_moves, player2_moves, current_player, board)
-    if current_player == "player1"
-      player1_moves << the_move
-    else
-      player2_moves << the_move
-    end
-    board = update_board(board, the_move, current_player)
-    show_board(board)
-    current_player = current_player == "player1" ? "player2" : "player1"
-    draw?(board)
-  end
-end
-
-def main(board)
+def tic_tac_toe
   while play_game?
-    tic_tac_toe(board, play_computer?)
-    board = board_reset
+    board = (1 .. 9).to_a
+    cpu_player = play_computer?
+    player1_moves = Set.new
+    player2_moves = Set.new
+    current_player = "player1"
+    until game_complete?(player1_moves, player2_moves, board)
+      show_board(board)
+      the_move = player_move(cpu_player, player1_moves, player2_moves, current_player, board)
+      if current_player == "player1"
+        player1_moves << the_move
+      else
+        player2_moves << the_move
+      end
+      board = update_board(board, the_move, current_player)
+      current_player = current_player == "player1" ? "player2" : "player1"
+    end
   end
 end
 
-main(board)
+tic_tac_toe
 
 binding.pry
 
