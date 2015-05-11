2f98b5e7b2e0217bdc9e9cd6f64ae08a9b1aa0f2
Rickard Sunden <rickard.sunden@outlook.com>
2015-05-11 14:16:36 -0400

    Did a bit of refactoring, still stable

--- 05-08-2015/tic-tac-toe.rb
+++ 05-08-2015/tic-tac-toe.rb
@@ -3,55 +3,44 @@ require 'set'
 
 board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 winning_combos = board.each_slice(3).to_a + [[1,4,7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-winning_combos = winning_combos.to_set
+WINS = winning_combos.to_set
 
 def play_game?
-  result = nil
+  question = "Do you want to play tic-tac-toe? y/n"
+  user_input = question_answer(question, /^[yn]$/i)
+  result = user_input == "Y" ? true : false
+end
+
+def question_answer(question, regex)
   puts dash_line
-  puts "Do you want to play tic-tac-toe? y/n"
+  puts question
   puts dash_line
-  user_input = gets.chomp
-  until ['y','n'].include?(user_input.downcase)
+  result = gets.chomp
+  until result =~ regex
     puts dash_line
-    puts "Sorry I didn't understand...please enter again: y/n"
+    puts "Sorry your input is not valid..."
     puts dash_line
-    user_input = gets.chomp
+    result = gets.chomp
   end
-  if user_input == "y"
-    result = true
-  else
-    result = false
-  end
-  result
+  result.upcase
 end
 
 def play_computer?
-  result = nil
-  puts dash_line
-  puts "Do you want to play against another player or the computer? p/c"
-  puts dash_line
-  user_input = gets.chomp
-  until ['p','c'].include?(user_input.downcase)
-    puts dash_line
-    puts "Sorry I didn't understand...please enter again: p/c"
-    puts dash_line
-    user_input = gets.chomp
-  end
-  if user_input == "p"
-    result = false
+  question = "Do you want to play against another player or the computer? p/c"
+  user_input = question_answer(question, /^[pc]$/i)
+  if user_input == "P"
     puts dash_line
     puts "You have chosen to play an another player!"
     puts dash_line
+    result = false
   else
-    result = true
     puts dash_line
     puts "You have chosen to play the computer!"
     puts dash_line
+    result = true
   end
-  result
 end
 
-#visual formatting
 def show_board(board)
   puts
   board = board.each_slice(3).to_a
@@ -65,33 +54,15 @@ def dash_line
   result = "-"*80
 end
 
-def dash_length
-  dash_line.length
-end
-
 def whose_turn?(turn_count)
-  result = nil
-  if turn_count.odd?
-    result = "player1"
-  else
-    result = "player2"
-  end
-  result
+  turn_count.odd? ? "player1" : "player2"
 end
 
-def get_player_input(player, board)
-  puts dash_line
-  puts "Pick a move #{player}: "
-  puts dash_line
-  user_input = gets.chomp.to_i
-  puts dash_line
-  until input_valid?(user_input, board)
-    puts dash_line
-    puts "Invalid input..."
-    puts dash_line
-    user_input = gets.chomp.to_i
-  end
-  user_input
+def choose_move(player, board)
+  prompt = "Pick a move #{player}"
+  available_moves = board.select { |x| x.is_a? Integer}
+  result = question_answer(prompt, /^#{available_moves}$/i)
+  result.to_i
 end
 
 def choose_computer_move(player1_moves, player2_moves)
@@ -101,23 +72,9 @@ def choose_computer_move(player1_moves, player2_moves)
   result = available_moves.to_a.sample
 end
 
-def input_valid?(player_input, board)
-  result = nil
-  if (1..9).to_a.include?(player_input)
-    if board.include?(player_input) == false
-      result = false
-    else
-      result = true
-    end
-  else
-    result = false
-  end
-  result
-end
-
 def update_board(board, player_input, current_player)
-    board[board.index(player_input)] = player_letter(current_player)
-    board
+  board[board.index(player_input)] = player_letter(current_player)
+  board
 end
 
 def player_letter(current_player)
@@ -128,9 +85,9 @@ def player_letter(current_player)
   end
 end
 
-def player_win?(player1_moves, player2_moves, winning_combos)
+def player_win?(player1_moves, player2_moves)
   result = nil
-  winning_combos.each do |combo|
+  WINS.each do |combo|
     if combo.to_set.subset?(player1_moves)
       result = "Player 1"
     elsif combo.to_set.subset?(player2_moves)
@@ -140,9 +97,9 @@ def player_win?(player1_moves, player2_moves, winning_combos)
   result
 end
 
-def game_complete?(turn_count, player1_moves, player2_moves, winning_combos)
+def game_complete?(turn_count, player1_moves, player2_moves)
   result = nil
-  player_win = player_win?(player1_moves, player2_moves, winning_combos)
+  player_win = player_win?(player1_moves, player2_moves)
   if player_win == "Player 1"
     puts dash_line
     puts "#{player_win} wins!"
@@ -175,10 +132,10 @@ def player_move(is_computer, player1_moves, player2_moves, current_player, board
       puts "Computer has picked number #{result}!"
       puts dash_line
     else
-      result = get_player_input(current_player, board)
+      result = choose_move(current_player, board)
     end
   else
-    result = get_player_input(current_player, board)
+    result = choose_move(current_player, board)
   end
   result
 end
@@ -187,12 +144,12 @@ def board_reset
   board = (1..9).to_a
 end
 
-def tic_tac_toe(board, winning_combos, is_play_computer)
+def tic_tac_toe(board, is_play_computer)
   player1_moves = Set.new
   player2_moves = Set.new
   turn_count = 1
   show_board(board)
-  until game_complete?(turn_count, player1_moves, player2_moves, winning_combos)
+  until game_complete?(turn_count, player1_moves, player2_moves)
     current_player = whose_turn?(turn_count)
     the_move = player_move(is_play_computer, player1_moves, player2_moves, current_player, board)
     if current_player == "player1"
@@ -206,11 +163,14 @@ def tic_tac_toe(board, winning_combos, is_play_computer)
   end
 end
 
-def main(board, winning_combos)
+def main(board)
   while play_game?
-    tic_tac_toe(board, winning_combos, play_computer?)
+    tic_tac_toe(board, play_computer?)
     board = board_reset
   end
 end
 
-main(board, winning_combos)
+main(board)
+
+binding.pry
+
