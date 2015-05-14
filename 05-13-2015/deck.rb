5412bef5db7da6d966b8f79ea4354281db3a2a88
Rickard Sunden <rickard.sunden@outlook.com>
2015-05-13 21:46:43 -0400

    Attempted hard mode

--- 05-13-2015/deck.rb
+++ 05-13-2015/deck.rb
@@ -4,8 +4,8 @@ module IronYardGames
   
   class Card
 
-  RANKS = (2..10).to_a + [:J, :Q, :K, :A]
-  SUITS = [:diamonds, :clubs, :spades, :hearts]
+  RANKS = (2..10).to_a + ["J", "Q", "K", "A"]
+  SUITS = ["d", "c", "s", "h"]
 
   RANK_VALUES = {J: 11, Q: 12, K: 13, A: 14}
 
@@ -49,9 +49,6 @@ module IronYardGames
     
     include Enumerable
 
-    RANKS = (2..10).to_a + ["J", "Q", "K", "A"]
-    SUITS = ["d", "c", "s", "h"]
-
     attr_reader :deck
 
     def initialize
@@ -60,8 +57,8 @@ module IronYardGames
 
     def get_deck
       result = []
-      RANKS.each do |rank|
-        SUITS.each do |suit|
+      Card::RANKS.each do |rank|
+        Card::SUITS.each do |suit|
           result << [rank,suit]
         end
       end
@@ -91,6 +88,43 @@ module IronYardGames
       @deck.each do |card|
         yield card
       end
+    end
+
+  end
+
+  class Hand
+
+    include Comparable
+
+    def initialize(hand)
+      @hand = split_to_array(hand)
+      @hand = self.create_card_object
+
+    end
+
+    def split_to_array(hand)
+      result = hand.split(" ")
+      result.map do |item|
+        item.chars
+      end
+    end
+
+    def create_card_object
+      result = []
+      self.each do |item|
+        result << IronYardGames::Card.new(item[0],item[1])
+      end
+      result
+    end
+
+    def each(&block)
+      @hand.each do |card|
+        yield card
+      end
+    end
+
+    def <=>(other)
+      self.value <=> other.value
     end
 
   end
