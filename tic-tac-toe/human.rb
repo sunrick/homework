6c4141948270c018605ac7bb73350138b81bd08a
Rickard Sunden <rickard.sunden@outlook.com>
2015-05-14 16:28:12 -0400

    Removed input validation, created validation class

--- tic-tac-toe/human.rb
+++ tic-tac-toe/human.rb
@@ -1,21 +1,14 @@
 require 'pry'
+require './validation'
 
 class Human
+
+  include Validation
 
   attr_reader :player_name
 
   def initialize
     @player_name = self.ask_for_input("What is your name?", /^[xo]$/i)
-  end
-
-  def ask_for_input(question, regex)
-    puts question
-    result = gets.chomp
-    until result =~ regex
-      puts "Sorry your input is not valid..."
-      result = gets.chomp
-    end
-    result.upcase
   end
 
   def ask_for_move(available_moves)
