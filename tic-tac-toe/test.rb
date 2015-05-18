180a7a4efc5afb48f0082566916f3cc4852b0e83
Rickard Sunden <rickard.sunden@outlook.com>
2015-05-18 17:04:24 -0400

    Added test cases, all pass

--- tic-tac-toe/test.rb
+++ tic-tac-toe/test.rb
@@ -15,7 +15,16 @@ class GameTest < MiniTest::Test
       game = Game.new(Human.new("Rick"),Computer.new("John"),[3,3])
       assert_instance_of Game, game
     end
-    
+
+    def test_human_player_can_move
+      game = Game.new(Human.new("Rick"),Computer.new("John"),[3,3])
+      assert game.choose_move
+    end
+
+    def test_computer_player_can_move
+      game = Game.new(Computer.new("Rick"),Computer.new("John"),[3,3])
+      assert game.choose_move
+    end
 end
 
 class BoardTest < MiniTest::Test
@@ -43,7 +52,31 @@ class BoardTest < MiniTest::Test
 end
 
 class ComputerTest < MiniTest::Test
+
+  def test_computer_can_choose_move
+    computer = Computer.new("John")
+    assert computer.get_move([1, 2, 3, 4, 'x', 'x', 'x', 8, 9])
+  end
+
+  def test_computer_cannot_choose_move
+    computer = Computer.new("John")
+    refute computer.get_move([])
+  end
+
 end
 
 class HumanTest < MiniTest::Test
+
+  def can_create_human
+    human = Human.new("Sup")
+    assert_instance_of Human, human
+    human = Human.new
+    refute_instance_of Human, human
+  end
+
+  def test_human_can_choose_move
+    human = Human.new("John")
+    assert human.get_move([1, 2, 3, 4, 4, 5, 6, 8, 9])
+  end
+
 end
