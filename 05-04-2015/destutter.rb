67afd5c9b7418a331e1a7fa73f9631a2dfc2665a
Rickard Sunden <rickard.sunden@outlook.com>
2015-09-21 12:01:19 -0400

    Changed function

--- 05-04-2015/destutter.rb
+++ 05-04-2015/destutter.rb
@@ -1,15 +1,14 @@
 def destutter(seq)
 	result = []
-  seq = seq.split(", ")
 	last = nil
 	seq.each do |item|
 		result.push(item) unless last == item
 		last = item
 	end
-	result
+  result
 end
 
 puts "Hey give me a list of numbers to destutter! Ex 1, 2, 3. Make sure format is correct"
 input = gets.chomp
 puts "Original array: #{input}"
-puts "New array #{destutter(input).join(', ')}"
+puts "New array #{destutter(input.split(", ")).join(', ')}"
