6b0bf8047dfe368d090d9f0b8547f4e453227011
Rickard Sunden <rickard.sunden@outlook.com>
2015-09-21 12:23:06 -0400

    Best game ever

--- 05-04-2015/randomnumber.rb
+++ 05-04-2015/randomnumber.rb
@@ -1,29 +1,29 @@
-# random generator program 1-100
-
-def get_guess
-	puts "Guess a number between 1 and 100:"
-	gets.chomp.to_i
-end
-
-def random_number_game
-
-  random_number = rand(100)
-  user_guess = nil
-
-  user_guess = get_guess
-
-  until get_guess == random_number
-    if user_guess > random_number
-  	  puts "Guess Lower"
-  	elsif user_guess < random_number
-  	  puts "Guess Higher"
-  	end
-  	user_guess = get_guess
+def play
+  lets_play = "Y"
+  puts "Welcome to the world's best random number guessing game!"
+  while lets_play == "Y"
+    puts "You'll have to guess a number between 1 and whatever number you choose."
+    puts "Up to what number do you want to guess?"
+    number = gets.chomp
+    game(number)
+    puts "OK NICE YOU WIN! Want to play again? Y/N "
+    lets_play = gets.chomp.upcase
   end
-
-  puts "Good job man! The number was: " + random_number.to_s
-  puts "Your guess was: " + user_guess.to_s
-
 end
 
-random_number_game
+def game(number)
+  puts "OK WE'RE READY TO ROCK"
+  random_number = rand(number.to_i)
+  puts "WHATS YOUR GUESS BETWEEN 1-#{number}?\n"
+  guess = gets.chomp.to_i
+  while guess != random_number
+    if guess > random_number
+      puts "HEY GUESS LOWER!"
+    elsif guess < random_number
+      puts "HEY GUESS HIGHER!"
+    end
+    guess = gets.chomp.to_i
+  end
+end
+
+play
